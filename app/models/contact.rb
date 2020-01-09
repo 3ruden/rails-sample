713ab0e978c0c045ab086a686688bc7c705daf4f
micchan <p1r2yy9knr@gmail.com>
2020-01-10 01:34:19 +0900

    Railsの日本語化対応

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,5 @@
 class Contact < ApplicationRecord
-  validates :content, length: { in: 1..140}, presence: true
+  validates :content, 
+    length: { in: 1..140, message: 'は1文字以上140文字以下で入力してください。' },
+    presence: { message: 'を入力してください。' }
 end
